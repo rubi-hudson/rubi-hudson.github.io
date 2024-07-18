05820d69e6e3cb67bb8315d717c9bf55ddc1393d
Rubi Hudson <151895189+rubi-hudson@users.noreply.github.com>
2024-07-17 20:05:52 -0400

    Update hideshow.js

--- js/hideshow.js
+++ js/hideshow.js
@@ -7,14 +7,3 @@ function expandIt(whichAb,whichMore) {
 		whichMore.innerHTML="&#9656 Abstract";
 	}
 }
-
-
-function expandItGeneral(whichAb,whichMore) {
-	if (whichAb.style.display=='none') {
-		whichAb.style.display = (whichAb.style.display == "none" ) ? "" : "none";
-		whichMore.innerHTML="&#9662 Expand";	}
-	else {
-		whichAb.style.display ='none';
-		whichMore.innerHTML="&#9656 Expand";
-	}
-}
