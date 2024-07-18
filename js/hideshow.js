c066bfe81617c2d84bab9c3fd3d3d38434ae4969
Rubi Hudson <151895189+rubi-hudson@users.noreply.github.com>
2024-07-17 20:07:36 -0400

    Delete js directory

--- js/hideshow.js
+++
@@ -1,9 +0,0 @@
-function expandIt(whichAb,whichMore) {
-	if (whichAb.style.display=='none') {
-		whichAb.style.display = (whichAb.style.display == "none" ) ? "" : "none";
-		whichMore.innerHTML="&#9662 Abstract";	}
-	else {
-		whichAb.style.display ='none';
-		whichMore.innerHTML="&#9656 Abstract";
-	}
-}
